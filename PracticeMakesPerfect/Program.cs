51cf3f9535f3413ca82bf8012b6c9340c1eba999
MountDumb <torb3755@edu.eal.dk>
2017-02-09 19:14:53 +0100

    fdgfd

--- PracticeMakesPerfect/Program.cs
+++ PracticeMakesPerfect/Program.cs
@@ -18,15 +18,40 @@ namespace PracticeMakesPerfect
 
         void Run()
         {
-            Console.WriteLine(logic.CompareTwoInts(2, 3));
-            Console.WriteLine(logic.CompareThreeInts(2, 3, 4));
-            Console.WriteLine(logic.CompareTwoChars('a', 'd'));
-            Console.WriteLine(logic.CalcModulo(21, 3));
-            Console.WriteLine("the letter 'u' occurs {0} times", logic.CountOccurrencesOfCharInString("bubbidibubub", 'u'));
-            //game.NewGame();
-            logic.CheckStringForEnglishLetters("ab%cDEF{}Ghijk'lMN!!");
-            logic.Swapper(-3, 4);
-            Console.ReadKey();
+            bool running = true;
+            string choice;
+
+            while (running)
+            {
+                Console.Clear();
+                Console.WriteLine("1: Logic" + Environment.NewLine + "2: Guessing Game" + Environment.NewLine + "x: Exit");
+                choice = Console.ReadLine();
+                switch (choice.ToLower())
+                {
+                    case "1":
+                        Console.WriteLine(logic.CompareTwoInts(2, 3));
+                        Console.WriteLine(logic.CompareThreeInts(2, 3, 4));
+                        Console.WriteLine(logic.CompareTwoChars('a', 'd'));
+                        Console.WriteLine(logic.CalcModulo(21, 3));
+                        Console.WriteLine("the letter 'u' occurs {0} times", logic.CountOccurrencesOfCharInString("bubbidibubub", 'u'));
+                        logic.CheckStringForEnglishLetters("ab2c4&%abe{}||");
+                        logic.Swapper(-3, 4);
+                        Console.ReadKey();
+                        break;
+                    case "2":
+                        game.NewGame();
+                        Console.ReadKey();
+                        break;
+                    case "x":
+                        running = false;
+                        break;
+                    default:
+                        break;         
+                    
+                }
+                   
+            }
+            
         }
     }
 }
